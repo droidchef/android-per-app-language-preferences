0d6d123cb7e8ce41063c1e7f32faf346d213616b
Ishan Khanna <ishan1604@gmail.com>
2022-04-29 12:32:41 -0700

    Adds a MainActivity class
    This will allow us to build the logic for displaying a greeting, adding a drop down for language selection and changing languages using the new api

---
+++ app/src/main/java/dev/droidchef/samples/languageselector/MainActivity.kt
@@ -0,0 +1,10 @@
+package dev.droidchef.samples.languageselector
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
